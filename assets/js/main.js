364fd63843fb53708284767c53257774af10042a
adarsh <adkmr08@gmail.com>
2020-11-02 19:44:53 +0530

    changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -287,3 +287,11 @@ $('.video-modal').magnificPopup({
 }
 
 });
+
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function () {
+ 
+  links.classList.toggle("show-links");
+});
